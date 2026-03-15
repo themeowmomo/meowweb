34700a677ad63dd85a3b6b38b04c76456266d6c2
meow momo <themeowmomo@gmail.com>
2026-03-15 04:00:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7209337500-19893",
+  "appId": "1:589877035980:web:d590c70b7b1ff9e6479369",
+  "apiKey": "AIzaSyDGH8ShuourzemuqHJYjaCmg9GvXW3-UU0",
+  "authDomain": "studio-7209337500-19893.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "589877035980"
+};
